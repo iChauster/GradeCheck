a26065248d87a5e865f51f62925dc1599270c9d6
Ivan Chau <ivanmchau@gmail.com>
2016-02-20 13:51:54 -0500

    Initial Commit

---
+++ GradeCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradeCheck
+//
+//  Created by Ivan Chau on 2/20/16.
+//  Copyright © 2016 Ivan Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
