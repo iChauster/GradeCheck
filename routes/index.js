646bfbc126bb298a29e3d2f3c595e4386a20cffd
Ivan Chau <ivanmchau@gmail.com>
2019-01-30 23:31:42 -0500

    mp switch

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ var push = require('../controllers/push')
 
 */
 
-var markingPeriod = "MP1";
+var markingPeriod = "MP2";
 /* GET home page. */
 app.get('/', function(req, res, next) {
   console.log(req.headers);
