1ad0552f587e25263126bf7f20be00e2303d9a4f
Ivan Chau <ivanmchau@gmail.com>
2019-09-04 21:16:37 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ var push = require('../controllers/push')
 
 */
 
-var markingPeriod = "MP2";
+var markingPeriod = "MP1";
 /* GET home page. */
 app.get('/', function(req, res, next) {
   console.log(req.headers);
